bb4973605ce57e36edea9a433d086d70aff0f8b4
Michał Korman <michal.korman@icloud.com>
2021-08-14 22:51:55 +0200

    allow redis connection to be tunneled through TLS

--- src/providers/redis.ts
+++ src/providers/redis.ts
@@ -3,13 +3,15 @@ import { Tedis } from 'tedis';
 import {
     RedisHost,
     RedisPort,
-    RedisPassword
+    RedisPassword,
+    RedisTLS
 } from './config';
 
 const redisClient = new Tedis({
     host: RedisHost,
     port: RedisPort,
-    password: RedisPassword
+    password: RedisPassword,
+    tls: RedisTLS ? ({ key: undefined, cert: undefined }) : undefined
 });
 
 export const testRedisConnection = (): Promise<void> => {
