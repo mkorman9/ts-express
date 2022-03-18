ad8ac18f9538838050d5b5c753aab05afca51f81
Michał Korman <michal.korman@icloud.com>
2022-03-18 13:43:02 +0100

    implement createChannel for amqp

--- src/common/providers/amqp.ts
+++ src/common/providers/amqp.ts
@@ -1,6 +1,22 @@
 import amqp from 'amqplib';
 import config, { ConfigurationError } from './config';
 
+interface QueueProps {
+  name: string;
+  options: amqp.Options.AssertQueue;
+}
+
+interface ExchangeProps {
+  name: string;
+  type: string;
+  options: amqp.Options.AssertExchange;
+}
+
+interface ChannelProps {
+  queues?: QueueProps[];
+  exchanges?: ExchangeProps[];
+}
+
 let connection: amqp.Connection = null;
 
 export const initAMQP = async () => {
@@ -19,6 +35,33 @@ export const closeAMQP = async () => {
   if (connection) {
     await connection.close();
   }
+};
+
+export const createChannel = async (props?: ChannelProps): Promise<amqp.Channel> => {
+  if (!connection) {
+    return {} as amqp.Channel;
+  }
+
+  const channel = await connection.createChannel();
+  const queuesToDeclare = props?.queues || [];
+  const exchangesToDeclare = props?.exchanges || [];
+
+  for (const queue of queuesToDeclare) {
+    await channel.assertQueue(
+      queue.name,
+      queue.options
+    );
+  }
+
+  for (const exchange of exchangesToDeclare) {
+    await channel.assertExchange(
+      exchange.name,
+      exchange.type,
+      exchange.options
+    );
+  }
+
+  return channel;
 };
 
 export default connection;
