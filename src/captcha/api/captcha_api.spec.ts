cf39f422866aceffa14d69c7adaebe1f0c479da1
Michał Korman <michal.korman@icloud.com>
2022-04-21 19:19:49 +0200

    add more tests for captcha api

--- src/captcha/api/captcha_api.spec.ts
+++ src/captcha/api/captcha_api.spec.ts
@@ -60,6 +60,74 @@ describe('Captcha API Tests', () => {
     expect(response.body).eql(imageData);
   });
 
+  it('should use specified image size for existing captcha', async () => {
+    // given
+    const captchaId = '76c7ca82-6d5c-428c-8088-147413891f59';
+    const imageData = Buffer.of(1, 2, 3, 4);
+    const width = 200;
+    const height = 100;
+    const getImageMock = sinon.stub(captchaProvider, 'getImage')
+      .returns(Promise.resolve(imageData));
+
+    // when
+    const response = await chai.request(app)
+      .get(`/api/v1/captcha/image/${captchaId}`)
+      .query({
+        width,
+        height
+      });
+
+    // then
+    getImageMock.restore();
+    expect(getImageMock.callCount).equal(1);
+    expect(getImageMock.lastCall.args).eql([captchaId, { width, height }]);
+
+    expect(response.status).equal(200);
+    expect(response.body).eql(imageData);
+  });
+
+  it('should use default image size when specified one is out of bounds', async () => {
+    // given
+    const captchaId = '76c7ca82-6d5c-428c-8088-147413891f59';
+    const imageData = Buffer.of(1, 2, 3, 4);
+    const width = 1200;
+    const height = -10;
+    const getImageMock = sinon.stub(captchaProvider, 'getImage')
+      .returns(Promise.resolve(imageData));
+
+    // when
+    const response = await chai.request(app)
+      .get(`/api/v1/captcha/image/${captchaId}`)
+      .query({
+        width,
+        height
+      });
+
+    // then
+    getImageMock.restore();
+    expect(getImageMock.callCount).equal(1);
+    expect(getImageMock.lastCall.args).eql([captchaId, { width: DefaultImageWidth, height: DefaultImageHeight }]);
+
+    expect(response.status).equal(200);
+    expect(response.body).eql(imageData);
+  });
+
+  it('should return 400 for invalid image size format', async () => {
+    // given
+    const captchaId = '76c7ca82-6d5c-428c-8088-147413891f59';
+
+    // when
+    const response = await chai.request(app)
+      .get(`/api/v1/captcha/image/${captchaId}`)
+      .query({
+        width: 'aaa',
+        height: 'bbb'
+      });
+
+    // then
+    expect(response.status).equal(400);
+  });
+
   it('should return 404 for non-existing captcha image', async () => {
     // given
     const captchaId = '76c7ca82-6d5c-428c-8088-147413891f59';
@@ -96,6 +164,45 @@ describe('Captcha API Tests', () => {
 
     expect(response.status).equal(200);
     // expect(response.body).eql(audioData);
+  });
+
+  it('should use specified audio language for existing captcha', async () => {
+    // given
+    const captchaId = '76c7ca82-6d5c-428c-8088-147413891f59';
+    const audioData = Buffer.of(1, 2, 3, 4);
+    const language = 'pl-PL';
+    const getAudioMock = sinon.stub(captchaProvider, 'getAudio')
+      .returns(Promise.resolve(audioData));
+
+    // when
+    const response = await chai.request(app)
+      .get(`/api/v1/captcha/audio/${captchaId}`)
+      .query({
+        language
+      });
+
+    // then
+    getAudioMock.restore();
+    expect(getAudioMock.callCount).equal(1);
+    expect(getAudioMock.lastCall.args).eql([captchaId, { language }]);
+
+    expect(response.status).equal(200);
+    // expect(response.body).eql(audioData);
+  });
+
+  it('should return 400 for invalid audio language', async () => {
+    // given
+    const captchaId = '76c7ca82-6d5c-428c-8088-147413891f59';
+
+    // when
+    const response = await chai.request(app)
+      .get(`/api/v1/captcha/audio/${captchaId}`)
+      .query({
+        language: 'ar-EG'
+      });
+
+    // then
+    expect(response.status).equal(400);
   });
 
   it('should return 404 for non-existing captcha audio', async () => {
