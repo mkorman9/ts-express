f789d9b4a959d8c2410d5a58f33cdc6e3b873274
Michał Korman <michal.korman@icloud.com>
2022-04-21 18:10:51 +0200

    use dayjs to parse date in zod

--- src/common/providers/validation.ts
+++ src/common/providers/validation.ts
@@ -1,10 +1,12 @@
+import dayjs from 'dayjs';
+
 export const parseDate = (arg: unknown): (Date | null | undefined) => {
   if (arg === null) {
     return null;
   }
 
   if (typeof arg === 'string' || arg instanceof Date) {
-    return new Date(arg);
+    return dayjs(arg).toDate();
   }
 
   return undefined;
