491edca741241c0ce54a5164ab30e1fd2b341772
Michał Korman <michal.korman@icloud.com>
2022-05-19 13:13:48 +0200

    update port

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -2,4 +2,4 @@
 
 yarn build
 docker build -t mkorman/ts-express .
-docker run -it --rm -p 5000:5000 -v $(pwd)/config-docker.yml:/app/config.yml:ro --net ts-express_default mkorman/ts-express
+docker run -it --rm -p 8080:8080 -v $(pwd)/config-docker.yml:/app/config.yml:ro --net ts-express_default mkorman/ts-express
